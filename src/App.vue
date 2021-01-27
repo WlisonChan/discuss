ae7a9b25d714c295e987e45d6da421cb24381485
william <chenzhiwei93@aliyum.com>
2021-01-27 16:54:01 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-button>按钮</v-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+//import css
+import 'vue-beauty/package/style/vue-beauty.min.css'
+
+//import components
+import vueBeauty from 'vue-beauty'
+Vue.use(vueBeauty)
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
